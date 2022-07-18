71a197ca9f270ece4e9004941716b063e778c6bf
yujial <yujia_liu_2@sfu.ca>
2022-07-17 20:09:08 -0700

    fix of get_not()

--- game.py
+++ game.py
@@ -48,7 +48,7 @@ def check(x, y):
 				else:
 					self.block[i][j].configure(state=DISABLED)
 
-def get_hadmard():
+def get_hadamard():
 	qr = qiskit.QuantumRegister(1) # call a quantum bit (or qubit)
 	cr = qiskit.ClassicalRegister(1) # call a clasical bit
 	program = qiskit.QuantumCircuit(qr, cr) # The quantum circuit is generated from the previous qubit and bit
@@ -59,29 +59,24 @@ def get_hadmard():
 	job = qiskit.execute( program, qiskit.BasicAer.get_backend('qasm_simulator') )
 	num0 = job.result().get_counts()['0']
 	num1 = job.result().get_counts()['1']
+	print(job.result().get_counts())
 	return num0 < num1
 
 def get_not():
 	qr = qiskit.QuantumRegister(1) # call a quantum bit (or qubit)
-	qr2 = qiskit.QuantumRegister(1) # call a quantum bit (or qubit)
 	cr = qiskit.ClassicalRegister(1) # call a clasical bit
-	program = qiskit.QuantumCircuit(qr, qr2, cr) # The quantum circuit is generated from the previous qubit and bit
+	program = qiskit.QuantumCircuit(qr, cr) # The quantum circuit is generated from the previous qubit and bit
 
 	# some problems here
-	program.cx(qr, qr2)
+	program.x(qr)
+	program.h(qr)
 
-	program.measure(qr, cr) # The qubit is measured and stored in the classic bit.
-
-	job = qiskit.execute( program, qiskit.BasicAer.get_backend('qasm_simulator') )
-	print(job.result().get_counts())
-
-	program.measure(qr,cr) # The qubit is measured and stored in the classic bit.
-
+	program.measure(qr, cr)
 
 	job = qiskit.execute( program, qiskit.BasicAer.get_backend('qasm_simulator') )
-	print(job.result.get_counts())
 	num0 = job.result().get_counts()['0']
 	num1 = job.result().get_counts()['1']
+	print(job.result().get_counts())
 	return num0 < num1
 
 
